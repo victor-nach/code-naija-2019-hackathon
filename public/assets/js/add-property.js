f299f572345431677fe2532362316cce3c677c2d
Zenith Wogwugwu <uzenith360@users.noreply.github.com>
2019-10-27 12:47:29 +0100

    add-mapbox

---
+++ public/assets/js/add-property.js
@@ -0,0 +1,32 @@
+(()=>{
+    document.querySelector('[get-location]').onclick = (e) =>{
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position=>{
+                alert(JSON.stringify(position));
+            }, err =>{
+                alert(`I'm going through changes!`);
+            });
+          } else {
+            alert('Location access is not supported by this device.');
+          }
+    };
+    
+    mapboxgl.accessToken = 'pk.eyJ1IjoiemVuaXRoeSIsImEiOiJjazFqM2hmbXYwNzZlM2RwN3NkMXc1aGoyIn0.v5eUZQH9iRoiVrhAf3eegA';
+    var map = new mapboxgl.Map({
+    container: 'googleMap',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [6.45407, 3.39467],
+    zoom: 3
+    });
+
+    // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(el)
+    .setLngLat([6.45407, 3.39467])
+    .addTo(map);
+
+    
+})();
